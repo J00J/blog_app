48983878c4a53bcc8b53db2e24c9b3bf2b9cfc91
J00J <mohobop@hotmail.com>
2015-03-08 23:48:05 +0200

    ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,45 +1,42 @@
-require 'rvm/capistrano' # Для работы rvm
-require 'bundler/capistrano' # Для работы bundler. При изменении гемов bundler автоматически обновит все гемы на сервере, чтобы они в точности соответствовали гемам разработчика. 
- 
-set :application, 'docs'
-set :rails_env, 'production'
-set :domain, 'jooj@23.97.133.163' # Это необходимо для деплоя через ssh. Именно ради этого я настоятельно советовал сразу же залить на сервер свой ключ, чтобы не вводить паролей.
-set :deploy_to, "/home/rails/apps/#{application}"
-set :use_sudo, false
-set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
-set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
- 
-set :rvm_ruby_string, 'ruby-2.0.0-p247' # Это указание на то, какой Ruby интерпретатор мы будем использовать.
- 
-set :scm, :git # Используем git.
-set :repository,  'git@github.com:J00J/blog_app.git' # Путь до вашего репозитария.
-set :branch, 'master' # Ветка из которой будем тянуть код для деплоя.
-set :deploy_via, :remote_cache # Указание на то, что стоит хранить кеш репозитария локально и с каждым деплоем лишь подтягивать произведенные изменения.
-set :ssh_options, { :forward_agent => true }
- 
-role :web, domain
-role :app, domain
-role :db,  domain, :primary => true
- 
-# интеграция rvm с capistrano настолько хороша, что при выполнении cap deploy:setup установит себя и указанный в rvm_ruby_string руби.
-before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby'
- 
-after 'deploy:update_code', 'deploy:migrate'
-after 'deploy:update_code', :roles => :app do
-  # Здесь для примера вставлен только один конфиг с приватными данными - database.yml. Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда. При каждом деплое создаются ссылки на них в нужные места приложения.
-  run "rm -f #{current_release}/config/database.yml"
-  run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
-end
- 
-# Далее идут правила для перезапуска unicorn
+lock '3.1.0'
+# application name and the github repository
+set :application, 'jooj'
+set :rails_env, "production"
+set :domain, "jooj@23.97.133.163:22"
+set :repo_url, 'git@github.com:YourGitHubName/YourRepoName.git'
+
+# describe the rbenv environment we are deploying into
+set :rbenv_type, :user
+set :rbenv_ruby, '2.0.0-p451'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
+# dirs we want symlinked to the shared folder
+# during deployment
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
 namespace :deploy do
+
+  desc 'Restart application'
   task :restart do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      #
+      # The capistrano-unicorn-nginx gem handles all this
+      # for this example
+    end
   end
-  task :start do
-    run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
   end
-  task :stop do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
-  end
+
 end
